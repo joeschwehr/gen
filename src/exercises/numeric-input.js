bcdaf6c1c08f8bb0004393e426bfa086001dd00f
Joe Schwehr <joe.schwehr@gmail.com>
2024-03-06 22:03:26 +0000

    added onChange, onFocus, onBlur

--- src/exercises/numeric-input.js
+++ src/exercises/numeric-input.js
@@ -2,13 +2,13 @@
 * Numeric Input Component
 *   HTML (initial state): <input type="text" class="c-numeric-input" />
 *   Requirement:
-*   - should only accept numeric value only such as: 1, 1.2, -5, or 1000
-*   - if user enters leading zero, or .  when user moves focus away from the input, it should
-*     change to correct format:
-*       .1 ==> 0.1 and 01 => 1
-*   - if user enter invalid character/value, HTML should change to this
+*   ✅ should only accept numeric value only such as: 1, 1.2, -5, or 1000
+*   ✅ if user enter invalid character/value, HTML should change to this
 *       <input type="text" class="c-numeric-input c-numeric-input--error" />
 *       <span class="c-numeric-input__error-msg">invalid input</span>
+*   ✅ if user enters leading zero, or .  when user moves focus away from the input, it should
+*     change to correct format:
+*       .1 ==> 0.1 and 01 => 1
 *   - if user enter valid value and move focus away from the input HTML should change to this:
 *       <input type="text" class="c-numeric-input c-numeric-input--valid" />
 *   - if user focus on the input or user clear value from the input,
@@ -21,8 +21,122 @@
 const NumericInput = {
   init: () => {
     document.querySelectorAll('.c-numeric-input').forEach(elem => {
-      console.log('TODO: Please see the above requirement for numeric input');
+      elem.addEventListener('input', NumericInput.onChange);
+      elem.addEventListener('focus', NumericInput.onFocus);
+      elem.addEventListener('blur', NumericInput.onBlur);
     });
+  },
+
+  onChange: (e) => {
+    sanitize(e);
+    validate(e);
+  },
+
+  onFocus: (e) => {
+
+  },
+
+  onBlur: (e) => {
+    let value = e.srcElement.value;
+    // *     change to correct format:
+    // *     01 => 1
+    if (value.startsWith('0') && value.length > 1 && value[1] !== '.') {
+      e.srcElement.value = value.slice(1);
+    }
+
+    // *     change to correct format:
+    // *       .1 ==> 0.1
+    if (value.startsWith('.'))
+      e.srcElement.value = '0' + value;
   }
 };
+
 document.addEventListener('DOMContentLoaded', NumericInput.init);
+
+const validate = (e) => {
+  const numericRegex = /^-?\d*\.?\d+$/ // - digit, ., digit
+  const incompleteNumericRegex = /^-?\d*\.?$/ // - digit, .
+
+  if (e.target.value === '' ||
+    e.target.value === '.' ||
+    e.target.value === '-' ||
+    incompleteNumericRegex.test(e.target.value)) {
+    removeErrorMessage(e);
+  } else if (!numericRegex.test(e.target.value)) {
+    addErrorMessage(e);
+  } else {
+    removeErrorMessage(e);
+  }
+}
+
+const sanitize = (e) => {
+  // remove invalid characters
+  e.target.value = e.target.value.replace(/[^0-9.-]/g, '');
+
+  // no hypen then dot
+  if (e.target.value[0] === '-') {
+    if (e.target.value[1] === '.')
+      e.target.value = e.target.value.slice(0, 1); // first character only
+  }
+
+  // no dot then hypen
+  // COMMENTED THIS TO TEST FOR ERRORS (".-"" will cause error)
+  // if (e.target.value[0] === '.') {
+  //   if (e.target.value[1] === '-')
+  //     e.target.value = e.target.value.slice(0, 1);
+  // }
+
+  // only one decimal point
+  if (e.target.value.includes('.')) {
+    if (e.target.value.match(/\./g).length > 1) {
+      e.target.value = e.target.value.slice(0, -1); // remove last character
+    }
+  }
+
+  // only one negative sign
+  if (e.target.value.includes('-')) {
+    if (e.target.value.match(/-/g).length > 1) {
+      e.target.value = e.target.value.slice(0, -1);
+    }
+  }
+
+  // only one leading zero
+  if (e.target.value.startsWith('0')) {
+    if (e.target.value.length > 1 && e.target.value[1] === '0') {
+      e.target.value = e.target.value.slice(0, 1);
+    }
+  }
+}
+
+const addErrorMessage = (e) => {
+
+  // add error class
+  e.target.classList.add('c-numeric-input--error');
+
+  // remove valid class if it exists
+  e.target.classList.remove('c-numeric-input--valid');
+
+  // check if error message already exists
+  const el = document.querySelector('.c-numeric-input__error-msg');
+
+  if (!el) {
+    const errorMsg = document.createElement('span');
+    errorMsg.classList.add('c-numeric-input__error-msg');
+    errorMsg.textContent = 'invalid input';
+    e.target.parentNode.appendChild(errorMsg);
+  }
+}
+
+const removeErrorMessage = (e) => {
+  // remove error class
+  e.target.classList.remove('c-numeric-input--error');
+
+  // add valid class
+  e.target.classList.add('c-numeric-input--valid');
+
+  // remove error message if it exists
+  const el = document.querySelector('.c-numeric-input__error-msg');
+  if (el) {
+    el.remove();
+  }
+}
